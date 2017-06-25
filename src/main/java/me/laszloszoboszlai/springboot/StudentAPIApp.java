dbf1305fccf190313d875784eadfc1c5a8d32c7f
Laszlo Szoboszlai <szlaci83@gmail.com>
2017-06-25 22:39:49 +0100

    setup devenv and boilerplate added

---
+++ src/main/java/me/laszloszoboszlai/springboot/StudentAPIApp.java
@@ -0,0 +1,13 @@
+package me.laszloszoboszlai.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAPIApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAPIApp.class, args);
+
+	}
+}
